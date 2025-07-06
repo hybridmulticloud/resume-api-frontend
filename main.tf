21d70c8bfc6edd3412f393945c4c5d10f13c1916
Kerem Kirci <mkerem.uk@gmail.com>
2025-07-06 23:02:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,48 +4,14 @@ data "aws_s3_bucket" "frontend" {
   bucket = "hybridmulti.cloud"
 }
 
-resource "aws_s3_bucket_website_configuration" "frontend_website" {
-  bucket = data.aws_s3_bucket.frontend.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
+output "s3_bucket_arn" {
+  value = data.aws_s3_bucket.frontend.arn
 }
 
-resource "aws_s3_bucket_ownership_controls" "ownership" {
-  bucket = data.aws_s3_bucket.frontend.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
+output "website_url" {
+  value = "https://${data.aws_s3_bucket.frontend.bucket_domain_name}"
 }
 
-resource "aws_s3_bucket_public_access_block" "frontend_public_block" {
-  bucket = data.aws_s3_bucket.frontend.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_policy" "frontend_policy" {
-  bucket = data.aws_s3_bucket.frontend.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject",
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = ["s3:GetObject"],
-        Resource  = "${data.aws_s3_bucket.frontend.arn}/*"
-      }
-    ]
-  })
+output "note" {
+  value = "This workspace does not manage the bucket. Deployment is handled via GitHub Actions."
 }
